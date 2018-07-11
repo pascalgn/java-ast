844f934d9bd7414a2e069b635b414fac9934a195
Uri Shaked <uri@urishaked.com>
2018-07-11 14:23:00 +0300

    fix: make library compatible with TypeScript 2.8

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
 import { JavaLexer } from './parser/JavaLexer';
-import { JavaParser } from './parser/JavaParser';
+import { CompilationUnitContext, JavaParser } from './parser/JavaParser';
+export { CompilationUnitContext };
 
 /**
  * Parses the given source code and returns the AST
  * @param source Java source code to parse
  */
-export function parse(source: string) {
+export function parse(source: string): CompilationUnitContext {
   const chars = new ANTLRInputStream(source);
   const lexer = new JavaLexer(chars);
   const tokens = new CommonTokenStream(lexer);
